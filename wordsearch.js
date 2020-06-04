e7d6753a4459f6a1b010081888d9da786fd56701
landon tipantiza <landontipantiza@gmail.com>
2020-06-04 01:43:03 +0000

    done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+    const verticalJoin = ['','','','','','','',''];
 
+    for (let i = 0; i < horizontalJoin.length; i++){
+        for(let j = 0; j < horizontalJoin[i].length; j++){
+            verticalJoin[j] += horizontalJoin[i][j];
+        }
+    }   
+    for (let string of horizontalJoin) {
+        if (string.includes(word)) { 
+            console.log("its true");
+            return true 
+        }
+    }
+    for (let string of verticalJoin) {
+        if (string.includes(word)) {
+            console.log("its true");
+            return true
+        }
+    }
+    return false;
+}
 module.exports = wordSearch
